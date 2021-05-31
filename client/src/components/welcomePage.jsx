5a9a07b7d5d2399d91ac49ea6d567d474ae55fc8
Patrick McConnell <pmcconnell13@gmail.com>
2021-05-31 08:05:54 -0700

    hi world

--- client/src/components/welcomePage.jsx
+++ client/src/components/welcomePage.jsx
@@ -12,14 +12,14 @@ function WelcomePage() {
             <img id="headshot" src={headshot}/>
           <div class="welcomeText">
             <div class="hello">
-              Hello World!
+              Hello
             </div>
-            {/* <div class="world">
+            <div class="world">
               World
             </div>
             <div class="excited">
               !
-            </div> */}
+            </div>
           </div>
           </div>
         </div>
