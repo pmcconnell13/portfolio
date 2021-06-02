524859c10556cae990ca150c0a20a3cc18757cd4
Patrick McConnell <pmcconnell13@gmail.com>
2021-06-02 14:08:45 -0700

    basic about me

--- client/src/components/aboutMe.jsx
+++ client/src/components/aboutMe.jsx
@@ -3,15 +3,17 @@ import React from 'react';
 function AboutMe() {
   return (
     <div class="section-container" id="aboutMe">
-      <div class="aboutName">
-        Patrick McConnell
+      <div class="aboutMeContainer">
+        <div class="aboutName">
+          Patrick McConnell
+        </div>
+        <div class="aboutTitle">
+          Software Engineer
+        </div>
+        <p class="aboutInfo">
+          I spent 2015-2020 traveling the world, working as a Dive Master in Australia, a Yoga instructor in Asia, and a luxury cycling guide in Europe. I met many Software Engineers along my journey and loved the potential to positively impact the world at scale. In 2020, my curiosity got the best of me and I dove head first into learning everything I could about coding. I haven't looked back since, and can't wait to join a progressive team looking to bring conscious change to the world.
+        </p>
       </div>
-      <div class="aboutTitle">
-        Software Engineer
-      </div>
-      <p class="aboutInfo">
-        "I spent 2015-2020 traveling the world, working as a Dive Master in Australia, a Yoga instructor in Asia, and a luxury cycling guide in Europe. I met many Software Engineers along my journey and loved the potential to create anything I could dream up. In 2020, my curiosity got the best of me and I dove head first into learning everything I could about coding. I haven't looked back since, and can't wait to join a progressive team looking to bring a positive change to the world.”
-      </p>
     </div>
   )
 }
