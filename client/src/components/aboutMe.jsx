dfb99c03aa11d4e2938cd02de5128602891341b7
Patrick McConnell <pmcconnell13@gmail.com>
2021-06-02 14:19:46 -0700

    line height changed

--- client/src/components/aboutMe.jsx
+++ client/src/components/aboutMe.jsx
@@ -11,7 +11,7 @@ function AboutMe() {
           Software Engineer
         </div>
         <p class="aboutInfo">
-          I spent 2015-2020 traveling the world, working as a Dive Master in Australia, a Yoga instructor in Asia, and a luxury cycling guide in Europe. I met many Software Engineers along my journey and loved the potential to positively impact the world at scale. In 2020, my curiosity got the best of me and I dove head first into learning everything I could about coding. I haven't looked back since, and can't wait to join a progressive team looking to bring conscious change to the world.
+          I spent 2015-2020 traveling the world, working as a Dive Master in Australia, a Yoga instructor in Asia, and a luxury cycling guide in Europe. I met many Software Engineers along my journey and loved the potential to positively impact the world at scale. In 2020, my curiosity got the best of me and I dove head first into becoming a software engineer. I haven't looked back since, and can't wait to join a progressive team looking to bring a conscious change to the world.
         </p>
       </div>
     </div>
